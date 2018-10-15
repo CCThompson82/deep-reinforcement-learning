b56aac6ae7010b5b05a06e49247cddf84d011313
CCThompson82 <christophercthompson82@gmail.com>
2018-10-15 19:11:36 +0100

    Feat: Use utils within taxi.agent class

--- src/lab-taxi/agent.py
+++ src/lab-taxi/agent.py
@@ -1,5 +1,7 @@
 import numpy as np
 from collections import defaultdict
+from src.utils.policies import policy_utils
+from src.utils.episodes import sarsa as sarsa_utils
 
 class Agent:
 
@@ -12,6 +14,24 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.episodes = 0
+
+        self.epsilon_config = {'decay_rate': 1e-4,
+                               'inflection_episode': 5000,
+                               'minimum_epsilon': 0.01,
+                               'ceiling_epsilon': 1.0}
+        self.alpha = 1e-3
+        self.gamma = 0.9
+
+    @property
+    def epsilon(self):
+        epsilon = policy_utils.get_sigmoid_epsilon(
+            episode_it=self.episodes,
+            decay_rate=self.epsilon_config['decay_rate'],
+            x50=self.epsilon_config['inflection_episode'],
+            floor=self.epsilon_config['minimum_epsilon'],
+            ceil=self.epsilon_config['ceiling_epsilon'])
+        return epsilon
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +44,11 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+
+        action = policy_utils.get_greedy_epsilon_action(
+            Q=self.Q, state=state, epsilon=self.epsilon, nb_A=self.nA)
+
+        return action
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +61,13 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        self.Q = sarsa_utils.expected_sarsa_update_step(Q=self.Q,
+                                                        state_0=state,
+                                                        action_0=action,
+                                                        state_1=next_state,
+                                                        reward0=reward,
+                                                        epsilon=self.epsilon,
+                                                        alpha=self.alpha,
+                                                        gamma=self.gamma,
+                                                        nb_A=self.nA)
+
--- src/utils/episodes/sarsa.py
+++ src/utils/episodes/sarsa.py
@@ -103,21 +103,24 @@ def run_expected_sarsa_episode(env, Q, epsilon, alpha, gamma):
 def generate_expected_sarsa_step(env, Q, state_0, action_0, epsilon, alpha, gamma):
     state_1, reward0, episode_finished, info = env.step(action_0)
 
-    state_action_values = Q[state_1]
-    max_action = np.argmax(state_action_values)
-    action_probabilities = np.full(env.nA, epsilon/env.nA)
-    action_probabilities[max_action] += 1-epsilon
-
-    G1 = np.sum(
-        [sa_value*action_prob for sa_value, action_prob in
-         zip(state_action_values, action_probabilities)])
-
-    current_value_estimate = Q[state_0][action_0]
-    discounted_return = G1 * gamma
-    delta_Q = (reward0 + discounted_return - current_value_estimate)
-    Q[state_0][action_0] = current_value_estimate + (alpha * delta_Q)
+    expected_sarsa_update_step(Q, action_0, alpha, epsilon, gamma, reward0, state_0, state_1, nb_A=env.nA)
 
     # choose an action here, even though not important for SA value estimation
     action_1 = policy.get_greedy_epsilon_action(Q, state_1, epsilon, env.nA)
 
     return Q, state_1, action_1, episode_finished, info
+
+
+def expected_sarsa_update_step(Q, action_0, alpha, epsilon, gamma, reward0, state_0, state_1, nb_A):
+    state_action_values = Q[state_1]
+    max_action = np.argmax(state_action_values)
+    action_probabilities = np.full(nb_A, epsilon / nb_A)
+    action_probabilities[max_action] += 1 - epsilon
+    G1 = np.sum(
+        [sa_value * action_prob for sa_value, action_prob in
+         zip(state_action_values, action_probabilities)])
+    current_value_estimate = Q[state_0][action_0]
+    discounted_return = G1 * gamma
+    delta_Q = (reward0 + discounted_return - current_value_estimate)
+    Q[state_0][action_0] = current_value_estimate + (alpha * delta_Q)
+    return Q
